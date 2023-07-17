3aada4949461e34a84992b69f3d21260b4722bbf
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-07-17 17:30:38 -0500

    fix: removed try catch

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -5,23 +5,15 @@ import { hotelService } from '@/services';
 
 async function getHotels(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
-  try {
-    const hotels = await hotelService.validateHotelRequest(userId);
-    res.status(httpStatus.OK).send(hotels);
-  } catch (err) {
-    res.sendStatus(httpStatus.BAD_REQUEST);
-  }
+  const hotels = await hotelService.validateHotelRequest(userId);
+  res.status(httpStatus.OK).send(hotels);
 }
 
 async function getHotelById(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
   const { id } = req.params;
-  try {
-    const hotel = await hotelService.validateHotelRequest(userId, Number(id));
-    res.status(httpStatus.OK).send(hotel);
-  } catch (err) {
-    res.sendStatus(httpStatus.BAD_REQUEST);
-  }
+  const hotel = await hotelService.validateHotelRequest(userId, Number(id));
+  res.status(httpStatus.OK).send(hotel);
 }
 
 const hotelController = {
