81587b13a5dd836bfdc3628d67ae1efa189e43ad
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-07-17 05:04:31 -0500

    fix: hotel repostory

---
+++ src/repositories/hotels-repository/index.ts
@@ -0,0 +1,19 @@
+import { prisma } from '@/config';
+
+async function getHotelsDB() {
+  return prisma.hotel.findMany();
+}
+
+async function getHotelByIdDB(id: number) {
+  return prisma.hotel.findFirst({
+    where: { id },
+    include: { Rooms: true },
+  });
+}
+
+const hotelRepository = {
+  getHotelsDB,
+  getHotelByIdDB,
+};
+
+export default hotelRepository;
